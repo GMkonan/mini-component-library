08f917c437c7b806207f325d298d90bd89f8c5ce
GMkonan <guilhemont@hotmail.com>
2022-01-04 02:01:42 -0300

    Add Note

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -34,6 +34,21 @@ const Breadcrumbs = ({children}) => (
         content: '/';
         margin-right: var(--spacing);
       }
+
+      //copied the note for reference (pretty interesting)
+      /* Note: The ideal version of this solution
+       * would instead use a transformed border,
+       * to avoid using a real character.
+       *
+       * For example, something like this:
+       *
+       *    content: '';
+       *    display: inline-block;
+       *    transform: rotate(15deg);
+       *    border-right: 1px solid;
+       *    margin-right: var(--spacing);
+       *    height: 0.8em;
+       */
     
     }
     `
