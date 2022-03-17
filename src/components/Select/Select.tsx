c81d4626ccb515025eb70cb1f3fe9e8751cf3786
GMkonan <guilhemont@hotmail.com>
2022-03-16 22:28:28 -0300

    Some updates to select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -9,10 +9,55 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <SelectWrapper value={value} onChange={onChange}>
+        {children}
+      </SelectWrapper>
+      <PresentationBit>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down" size={24} strokeWidth={1} />
+        </IconWrapper>
+      </PresentationBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+position: absolute;
+width: max-content;
+`
+
+const SelectWrapper = styled.select`
+position: absolute;
+opacity: 0;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+`
+
+const PresentationBit = styled.div`
+padding: 8px;
+color: ${COLORS.gray700};
+background-color: ${COLORS.gray50};
+border: none;
+border-radius: 4px;
+padding-right: 52px;
+
+${SelectWrapper}:hover + & {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+position: absolute;
+right: 10px;
+bottom: 0;
+top: 0;
+margin: auto;
+width: 24px;
+height: 24px;
+`
 
 export default Select;
